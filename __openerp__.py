8e96f2e3b78fcf7bf35c10c84699f425d4918088
morrillo <odoo@debian>
2015-08-08 13:58:15 +0000

    Added AFIP models

--- __openerp__.py
+++ __openerp__.py
@@ -2,7 +2,7 @@
     'name': 'MrLed - Permisos',
     'category': 'Accounting',
     'version': '0.1',
-    'depends': ['account','base'],
+    'depends': ['account','base','l10n_ar_invoice'],
     'data': [
         'security/groups.xml',
         'security/ir.model.access.csv',
