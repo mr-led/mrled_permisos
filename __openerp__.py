2c450035bcc80f2e4b72a1b7fd26ffafbee963a1
morrillo <odoo@debian>
2015-08-08 15:44:49 +0000

    Removed validate button from invoice form

--- __openerp__.py
+++ __openerp__.py
@@ -4,6 +4,7 @@
     'version': '0.1',
     'depends': ['account','base','l10n_ar_invoice'],
     'data': [
+        'account_invoice_view.xml',
         'security/groups.xml',
         'security/ir.model.access.csv',
     ],
