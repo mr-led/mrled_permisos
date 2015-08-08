144af29dead5357c5c643a28205e9ae1e047e2bc
morrillo <gustavo.orrillo@moldeo.coop>
2015-08-08 09:59:41 -0300

    First working commit

---
+++ __openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'MrLed - Permisos',
+    'category': 'Accounting',
+    'version': '0.1',
+    'depends': ['account','base'],
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'installable': True,
+}
